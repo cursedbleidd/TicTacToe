60d4b56438d8c4df32a56e539e4d602309394965
cursedbleidd <49333001+cursedbleidd@users.noreply.github.com>
2022-11-13 18:39:07 +0500

    New features + bug fixes
    Added double buffering, player roles for windows

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -27,8 +27,10 @@ HWND hwnd;               /* This is the handle for our window */
 HBRUSH hBrush;           /* Current brush */
 HANDLE bckGrnThread;
 HANDLE PaintThread;
-DWORD ThreadID[2];
-DWORD ExitCode[2];
+
+HDC hdc, hMdc;
+HBITMAP hBmpFrame;
+
 volatile bool bContinue = true;
 
 COLORREF line = 0x000000FF;
@@ -57,27 +59,37 @@ UINT FieldUpdate;
 UINT CloseAll;
 bool threadSuspended = false;
 
-bool CheckRow(int y)
+enum Role {
+    XOPLAYER,
+    XPLAYER,
+    OPLAYER,
+    OBSERVER
+};
+
+Role currentRole;
+Role previousRole;
+
+bool CheckRow(int y, TCHAR click)
 {
     bool win = true;
     for (int x = 0; x < N && win; x++)
-        win = FIELD[y * N + x] == FIELD[N * N];
+        win = FIELD[y * N + x] == click;
     return win;
 }
-bool CheckColumn(int x)
+bool CheckColumn(int x, TCHAR click)
 {
     bool win = true;
     for (int y = 0; y < N && win; y++)
-        win = FIELD[y * N + x] == FIELD[N * N];
+        win = FIELD[y * N + x] == click;
     return win;
 }
-bool CheckDiagonals()
+bool CheckDiagonals(TCHAR click)
 {
     bool win1 = true, win2 = true;
     for (int y = 0, x = 0; y < N && win1; y++, x++)
-        win1 = FIELD[y * N + x] == FIELD[N * N];
+        win1 = FIELD[y * N + x] == click;
     for (int y = 0, x = N - 1; y < N && win2; y++, x--)
-        win2 = FIELD[y * N + x] == FIELD[N * N];
+        win2 = FIELD[y * N + x] == click;
     return win1 || win2;
 }
 
@@ -124,18 +136,32 @@ void RunNotepad()
         NULL, NULL, NULL, FALSE, 0, NULL, NULL, &sInfo, &pInfo);
 }
 
-void ClickCalc(RECT r, int xPos, int yPos)
+void ClickCalc(RECT r, int xPos, int yPos, TCHAR result)
 {
     int x = xPos / (r.right / N), y = yPos / (r.bottom / N);
-    if (FIELD[y * N + x] == NULL) FIELD[y * N + x] = FIELD[N * N];
-    /* FIELD = "(0*N+1),..,(0*N+N-1),*/
-    /*(1*N+0),..,(1*N+N-1),*/
-    /*......,((N-1)*N+N-1)"*/
-    if (CheckRow(y) || CheckColumn(x) || CheckDiagonals())
+    if (result != FIELD[N * N])
     {
         const int BUFF_SIZE = 50;
         wchar_t msg[BUFF_SIZE];
         WCHAR player = (FIELD[N * N] == X) ? 'X' : 'O';
+        _snwprintf_s(msg, BUFF_SIZE, L"It's %c-player turn to move!", player);
+        MessageBox(hwnd, (LPCWSTR)msg, _T(""), MB_OK | MB_ICONSTOP);
+        return;
+    }
+    if (FIELD[y * N + x] == NULL)
+    {
+        FIELD[y * N + x] = FIELD[N * N];
+        FIELD[N * N] = (result == X) ? O : X;
+    }
+    else return;
+    /* FIELD = "(0*N+1),..,(0*N+N-1),*/
+    /*(1*N+0),..,(1*N+N-1),*/
+    /*......,((N-1)*N+N-1)"*/
+    if (CheckRow(y, FIELD[y * N + x]) || CheckColumn(x, FIELD[y * N + x]) || CheckDiagonals(FIELD[y * N + x]))
+    {
+        const int BUFF_SIZE = 50;
+        wchar_t msg[BUFF_SIZE];
+        WCHAR player = (FIELD[y * N + x] == X) ? 'X' : 'O';
         _snwprintf_s(msg, BUFF_SIZE, L"%c-player won!", player);
         MessageBox(hwnd, (LPCWSTR)msg, _T("Congratulations!"), MB_OK);
         PostMessage(HWND_BROADCAST, CloseAll, NULL, NULL);
@@ -191,42 +217,38 @@ void PaintLines(HDC hdc, int x, int y)
     DeleteObject(hPen);
 }
 
-DWORD WINAPI BackgroundThread(LPVOID t)
+void Paint()
 {
-    while (bContinue)
-    {
-        byte r = GetRValue(bckGrn), g = GetGValue(bckGrn), b = GetBValue(bckGrn); // calculates color in [85; 255]
-        bool r255 = r == 255, b255 = b == 255, g255 = g == 255;
-        bool r0 = r == 85, b0 = b == 85, g0 = g == 85;
-        if (r255 && g0 && !b255) nbckGrn = RGB(r, g, b += 85);
-        else if (!r0 && b255 && g0) nbckGrn = RGB(r -= 85, g, b);
-        else if (b255 && r0 && !g255) nbckGrn = RGB(r, g += 85, b);
-        else if (!b0 && g255 && r0) nbckGrn = RGB(r, g, b -= 85);
-        else if (g255 && b0 && !r255) nbckGrn = RGB(r += 85, g, b);
-        else if (r255 && b0 && !g0) nbckGrn = RGB(r, g -= 85, b);
-
-        Sleep(125);
-        bckGrn = nbckGrn;
-    }
-    ExitThread(0);
+    RECT clientRect;
+    GetClientRect(hwnd, &clientRect);
+    SelectObject(hMdc, hBmpFrame);
+    HorizontalGradient(hMdc, clientRect, bckGrn, nbckGrn);
+    PaintLines(hMdc, clientRect.right, clientRect.bottom);
+    PaintFigures(hMdc, clientRect.right, clientRect.bottom);
+    InvalidateRect(hwnd, NULL, TRUE);
 }
 
 DWORD WINAPI PaintFieldThread(LPVOID t)
 {
-
+    int i = 0;
     while (bContinue)
     {
-        InvalidateRect(hwnd, NULL, TRUE);
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        RECT clientRect;
-        GetClientRect(hwnd, &clientRect);
-        HorizontalGradient(hdc, clientRect, bckGrn, nbckGrn);
-        PaintLines(hdc, clientRect.right, clientRect.bottom);
-        PaintFigures(hdc, clientRect.right, clientRect.bottom);
-        EndPaint(hwnd, &ps);
-        DeleteObject(hdc);
-        Sleep(50);
+        if (i++ % 5 == 0)
+        {
+            byte r = GetRValue(bckGrn), g = GetGValue(bckGrn), b = GetBValue(bckGrn); // calculates color in [85; 255]
+            bool r255 = r == 255, b255 = b == 255, g255 = g == 255;
+            bool r0 = r == 85, b0 = b == 85, g0 = g == 85;
+            if (r255 && g0 && !b255) nbckGrn = RGB(r, g, b += 85);
+            else if (!r0 && b255 && g0) nbckGrn = RGB(r -= 85, g, b);
+            else if (b255 && r0 && !g255) nbckGrn = RGB(r, g += 85, b);
+            else if (!b0 && g255 && r0) nbckGrn = RGB(r, g, b -= 85);
+            else if (g255 && b0 && !r255) nbckGrn = RGB(r += 85, g, b);
+            else if (r255 && b0 && !g0) nbckGrn = RGB(r, g -= 85, b);
+        }
+        Paint();
+        if (i % 5 == 0)
+            bckGrn = nbckGrn;
+        Sleep(15);
     }
     ExitThread(0);
 }
@@ -234,6 +256,24 @@ DWORD WINAPI PaintFieldThread(LPVOID t)
 /*  This function is called by the Windows function DispatchMessage()  */
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    if (currentRole == OBSERVER)
+    {
+        if (FIELD[N * N + 1] == NULL)
+        {
+            FIELD[N * N + 1] = 1;
+            currentRole = previousRole = XPLAYER;
+        }
+        if (FIELD[N * N + 2] == NULL)
+        {
+            FIELD[N * N + 2] = 1;
+            currentRole = previousRole = OPLAYER;
+        }
+    }
+    if ((FIELD[N * N + 1] == NULL && currentRole == OPLAYER) || (FIELD[N * N + 2] == NULL && currentRole == XPLAYER))
+        currentRole = XOPLAYER;
+    else if (currentRole == XOPLAYER && (FIELD[N * N + 1] == 1 && FIELD[N * N + 2] == 1))
+        currentRole = previousRole;
+
     if (message == FieldUpdate)
     {
         InvalidateRect(hwnd, NULL, TRUE);
@@ -241,11 +281,13 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
     }
     if (message == CloseAll)
     {
-        PostMessage(hwnd, WM_DESTROY, NULL, NULL);
+        PostMessage(hwnd, WM_DESTROY, NULL, NULL); //TODO: Fix config writing
         return 0;
     }
     switch (message)                  /* handle the messages */
     {
+    case WM_ERASEBKGND:
+        return 1;
     case WM_MOUSEWHEEL: // yeah, i am insane, how could you tell?
     {
         byte r = GetRValue(line), g = GetGValue(line), b = GetBValue(line);
@@ -281,9 +323,9 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
         else if (GetKeyState(VK_SPACE) < 0)
         {
             if (!threadSuspended)
-                SuspendThread(bckGrnThread);
+                SuspendThread(PaintThread);
             else
-                ResumeThread(bckGrnThread);
+                ResumeThread(PaintThread);
 
             threadSuspended = !threadSuspended;
         }
@@ -306,20 +348,14 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
     case WM_LBUTTONDOWN:
     case WM_RBUTTONDOWN:
     {
-        TCHAR result = (message == WM_LBUTTONDOWN) ? X : O;
-        if (result != FIELD[N * N])
-        {
-            const int BUFF_SIZE = 50;
-            wchar_t msg[BUFF_SIZE];
-            WCHAR player = (FIELD[N * N] == X) ? 'X' : 'O';
-            _snwprintf_s(msg, BUFF_SIZE, L"It's %c-player turn to move!", player);
-            MessageBox(hwnd, (LPCWSTR)msg, _T(""), MB_OK | MB_ICONSTOP);
+        if (currentRole == OBSERVER)
             return 0;
-        }
+        TCHAR result = (message == WM_LBUTTONDOWN) ? X : O;
+        if (currentRole == XPLAYER && result == O) return 0;
+        if (currentRole == OPLAYER && result == X) return 0;
         RECT clientRect;
         GetClientRect(hwnd, &clientRect);
-        ClickCalc(clientRect, LOWORD(lParam), HIWORD(lParam));
-        FIELD[N * N] = (result == X) ? O : X;
+        ClickCalc(clientRect, LOWORD(lParam), HIWORD(lParam), result);
         PostMessage(HWND_BROADCAST, FieldUpdate, 0, 0);
     }
     return 0;
@@ -329,60 +365,52 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
         GetWindowRect(hwnd, &w);
         WIDTH = w.right - w.left;
         HEIGHT = w.bottom - w.top;
-        InvalidateRect(hwnd, NULL, TRUE);
+        RECT clientRect;
+        GetClientRect(hwnd, &clientRect);
+        hBmpFrame = CreateCompatibleBitmap(GetDC(hwnd), clientRect.right, clientRect.bottom);
+        Paint();
     }
     return 0;
     case WM_CREATE:
     {
+        RECT clientRect;
+        GetClientRect(hwnd, &clientRect);
+        hdc = GetDC(hwnd);
+        hMdc = CreateCompatibleDC(hdc);
+        hBmpFrame = CreateCompatibleBitmap(hdc, clientRect.right, clientRect.bottom);
+        SelectObject(hMdc, hBmpFrame);
         PaintThread = CreateThread( // thread for painting
             NULL,
             0,
             PaintFieldThread,
             NULL,
             0,
-            &ThreadID[0]
+            NULL
         );
         if (PaintThread == NULL)
         {
             _tprintf(_T("Could not create new thread. (%d).\n"),
                 GetLastError());
-            _getch();
-            return 1;
-        }
-        bckGrnThread = CreateThread(
-            NULL,
-            0,
-            BackgroundThread,
-            NULL,
-            0,
-            &ThreadID[1]
-        );
-        if (bckGrnThread == NULL)
-        {
-            _tprintf(_T("Could not create new thread. (%d).\n"),
-                GetLastError());
-            _getch();
+            
             return 1;
         }
     }
     return 0;
     case WM_PAINT:
+        PAINTSTRUCT ps;
+        RECT clientRect;
+        GetClientRect(hwnd, &clientRect);
+        hdc = BeginPaint(hwnd, &ps);
+        BitBlt(hdc, 0, 0, clientRect.right, clientRect.bottom, hMdc, 0, 0, SRCCOPY);
+        EndPaint(hwnd, &ps);
     return 0;
     case WM_DESTROY:
-    {
-        bContinue = false;
-        FILE* cnf;
-        fopen_s(&cnf, "config.txt", "w");
-        if (cnf == NULL)
-        {
-            _tprintf(_T("Could not create config file (%d).\n"),
-                GetLastError());
-            _getch();
-            return 1;
-        }
-        fprintf_s(cnf, "%d\n%d\n%d\n%d\n%d", N, WIDTH, HEIGHT, bckGrn, line);
-        fclose(cnf);
-    }
+    bContinue = false;
+    if (currentRole != OBSERVER)
+        FIELD[N * N + ((currentRole == XPLAYER) ? 1 : 2)] = NULL;
+    DeleteObject(hdc);
+    DeleteObject(hMdc);
+    DeleteObject(hBmpFrame);
     PostQuitMessage(0); /* send a WM_QUIT to the message queue */
     return 0;
     }
@@ -413,14 +441,14 @@ int main(int argc, char** argv)
             NULL,
             PAGE_READWRITE,
             0,
-            N * N + sizeof(TCHAR),
+            N * N + sizeof(TCHAR) * 3,
             szField
         );
         if (hFileMap == NULL)
         {
             _tprintf(_T("Could not create file mapping object (%d).\n"),
                 GetLastError());
-            _getch();
+            
             return 1;
         }
     }
@@ -439,7 +467,7 @@ int main(int argc, char** argv)
         {
             _tprintf(_T("Could not create config file (%d).\n"),
                 GetLastError());
-            _getch();
+            
             return 1;
         }
         fprintf_s(cnf, "%d\n%d\n%d\n%d\n%d", N, WIDTH, HEIGHT, bckGrn, line);
@@ -452,21 +480,29 @@ int main(int argc, char** argv)
         FILE_MAP_ALL_ACCESS,
         0,
         0,
-        N * N + sizeof(TCHAR)
+        N * N + sizeof(TCHAR) * 3
     );
     if (FIELD == NULL)
     {
         _tprintf(_T("Could not map view of file (%d).\n"),
             GetLastError());
 
-        _getch();
+        
         CloseHandle(hFileMap);
 
         return 1;
     }
     FIELD[N * N] = X;
-    srand(time(NULL));
 
+    currentRole = OBSERVER;
+   
+    if (FIELD[N * N + 1] == NULL && FIELD[N * N + 2] == NULL)
+    {
+        FIELD[N * N + 1] = 1;
+        currentRole = previousRole = XPLAYER;
+    }
+
+    srand(time(NULL));
     BOOL bMessageOk;
     MSG message;            /* Here message to the application are saved */
     WNDCLASS wincl = { 0 };         /* Data structure for the windowclass */
@@ -480,7 +516,7 @@ int main(int argc, char** argv)
     wincl.hInstance = hThisInstance;
     wincl.lpszClassName = szWinClass;
     wincl.lpfnWndProc = WindowProcedure;      /* This function is called by Windows */
-
+    wincl.hbrBackground = NULL;
     /* Register the window class, and if it fails quit the program */
     if (!RegisterClass(&wincl))
         return 0;
@@ -508,7 +544,7 @@ int main(int argc, char** argv)
         {
             _tprintf(_T("Suddenly, GetMessage failed (%d).\n"),
                 GetLastError());
-            _getch();
+            
             break;
         }
         /* Translate virtual-key message into character message */
@@ -516,7 +552,6 @@ int main(int argc, char** argv)
         /* Send message to WindowProcedure */
         DispatchMessage(&message);
     }
-
     /* Cleanup stuff */
     DestroyWindow(hwnd);
     UnregisterClass(szWinClass, hThisInstance);
@@ -525,5 +560,15 @@ int main(int argc, char** argv)
     CloseHandle(hFileMap);
     CloseHandle(bckGrnThread);
     CloseHandle(PaintThread);
+    fopen_s(&cnf, "config.txt", "w");
+    if (cnf == NULL)
+    {
+        _tprintf(_T("Could not create config file (%d).\n"),
+            GetLastError());
+
+        return 1;
+    }
+    fprintf_s(cnf, "%d\n%d\n%d\n%d\n%d", N, WIDTH, HEIGHT, bckGrn, line);
+    fclose(cnf);
     return 0;
 }
